90de796b951081f550f9cb13c638687fbca475dd
harsh dalal <dalalharsh1974@gmail.com>
2023-11-25 22:12:15 +0530

    first comit

---
+++ src/main/java/com/codewithme/quizapp/QuestionController.java
@@ -0,0 +1,14 @@
+package com.codewithme.quizapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+    @GetMapping("allQuestions")
+    public String getQuestions(){
+        return "Hi i Got here";
+    }
+}
